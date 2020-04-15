c1fa8c60c6f2e5c3991de87a194528e33c2b5301
Michael Lanzillo <lanzillo.michael1@gmail.com>
2020-04-15 13:13:27 -0500

    Completed first section of TODO items and adjusted namespace per Cruz's instruction

---
+++ ConsoleUI/Vehicle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleUI
+{
+    abstract class Vehicle 
+    {
+        public abstract string Year { get; set; }
+        public abstract string Make { get; set; }
+
+        public abstract string Model { get; set; }
+
+        public abstract void DriveAbstract();
+        
+        public virtual void DriveVirtual(Vehicle vehicle)
+        {
+            Console.WriteLine($"I am driving my vehicle, it is a {vehicle.Make} {vehicle.Model}. I bought it in {vehicle.Year}");
+        }
+        
+    }
+}
